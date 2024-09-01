d961226b0794e61c0a28cc802c840b8672a1a185
behappyleee <dhfhfkwhdk@naver.com>
2024-09-01 15:08:45 +0900

    studying commit

---
+++ src/main/kotlin/com/study/ordermanagement/OrderManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.study.ordermanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderManagementApplication>(*args)
+}
